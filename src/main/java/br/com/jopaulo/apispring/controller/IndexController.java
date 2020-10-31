457febe70133c3634d8ff1cbdbfe17688c024883
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-10-31 16:48:31 -0300

    Passando parâmetros para o serviço RESTfull

--- src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
+++ src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 	
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity<>("Projeto Spring Boot", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam (value = "nome", required = true, defaultValue = "Nome obrigatório necessário") String nome) {
+		System.out.println("Parâmetro sendo recebido: " + nome);
+		return new ResponseEntity("Nome do usuário: " + nome, HttpStatus.OK);
 	}
 
 }
