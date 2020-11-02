0c82d2670d9aeb36e7f9e36a62ec927d4805ae4e
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-11-02 20:50:17 -0300

    Retornando usuário em JSON para a tela

--- src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
+++ src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
@@ -1,5 +1,8 @@
 package br.com.jopaulo.apispring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +10,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.jopaulo.apispring.model.Usuario;
+
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init(@RequestParam (value = "nome", required = true, defaultValue = "Nome obrigatório necessário") String nome) {
-		System.out.println("Parâmetro sendo recebido: " + nome);
-		return new ResponseEntity("Nome do usuário: " + nome, HttpStatus.OK);
+	public ResponseEntity<Usuario> init() {
+		
+		Usuario usuario = new Usuario();
+		usuario.setId(50L);
+		usuario.setNome("João Paulo da Mata Mendes");
+		usuario.setLogin("jp_cbc@hotmail.com");
+		usuario.setSenha("1234");
+		
+		Usuario usuario2 = new Usuario();
+		usuario.setId(55L);
+		usuario.setNome("Adnari Mendes");
+		usuario.setLogin("adnari@hotmail.com");
+		usuario.setSenha("abcd");
+		
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+		
+		return new ResponseEntity(usuarios, HttpStatus.OK);
 	}
 
 }
