43f3bdc11f3580939d887da27c530410791d515f
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-11-04 16:47:03 -0300

    Criando a remoção com DELETE

--- src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
+++ src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,14 @@ public class IndexController {
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
 		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete (@PathVariable("id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+		
+		return "Usuário deletado com sucesso";
 	}
 	
 	@GetMapping(value = "/", produces = "application/json")
