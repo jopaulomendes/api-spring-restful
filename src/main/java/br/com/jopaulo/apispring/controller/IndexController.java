cb72b06d111c9021b5d33ced13fee8c5ae6ce0f5
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-11-09 13:22:10 -0300

    Implementando o END-POINT de atualização

--- src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
+++ src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
@@ -72,6 +72,10 @@ public class IndexController {
 	@PutMapping(value = "/", produces = "application/json")
 	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
 		
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		
 		Usuario  usuarioSalvo = usuarioRepository.save(usuario);
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);		
