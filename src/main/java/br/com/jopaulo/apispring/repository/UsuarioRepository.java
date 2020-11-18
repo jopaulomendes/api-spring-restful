91970ad8b940a58ff58e1177f9fff4268d409022
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-11-18 13:24:10 -0300

    Spring Security - Configurando o Repository e o Service

--- src/main/java/br/com/jopaulo/apispring/repository/UsuarioRepository.java
+++ src/main/java/br/com/jopaulo/apispring/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.com.jopaulo.apispring.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,8 @@ import br.com.jopaulo.apispring.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+	
+	@Query("select u from Usuario u where u.login = ?1")
+	Usuario findUserByLogin(String login);
 
 }
---
+++ src/main/java/br/com/jopaulo/apispring/service/ImplementacaoUserDetailsService.java
@@ -0,0 +1,31 @@
+package br.com.jopaulo.apispring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.jopaulo.apispring.model.Usuario;
+import br.com.jopaulo.apispring.repository.UsuarioRepository;
+
+@Service
+public class ImplementacaoUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Usuario usuario = usuarioRepository.findUserByLogin(username);
+		
+		if (usuario == null) {
+			throw new UsernameNotFoundException("Usuário não encontrado");
+		}
+		
+		return new User(usuario.getLogin(), usuario.getPassword(), usuario.getAuthorities());
+	}
+
+}
