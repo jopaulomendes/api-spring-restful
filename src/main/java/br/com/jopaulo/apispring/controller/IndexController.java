8eb514f4117341bfced51ff0a3ea2083f3a07e6f
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-11-24 22:02:15 -0300

    JSON Web Token (JWT) - Atualizando senha do usuário

--- src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
+++ src/main/java/br/com/jopaulo/apispring/controller/IndexController.java
@@ -79,6 +79,13 @@ public class IndexController {
 			usuario.getTelefones().get(pos).setUsuario(usuario);
 		}
 		
+		Usuario userTemporario = usuarioRepository.findUserByLogin(usuario.getLogin());
+		
+		if (!userTemporario.getSenha().equals(usuario.getSenha())) {
+			String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+			usuario.setSenha(senhaCriptografada);
+		}
+		
 		Usuario  usuarioSalvo = usuarioRepository.save(usuario);
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);		
