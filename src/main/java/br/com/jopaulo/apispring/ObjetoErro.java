dac33e29e6702f3eb73cd7190db5609bbde6ff6e
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-11-30 22:28:32 -0300

    Controle de erros da API com @ControllerAdvice, @RestControllerAdvice e @ExceptionHandler

---
+++ src/main/java/br/com/jopaulo/apispring/ObjetoErro.java
@@ -0,0 +1,24 @@
+package br.com.jopaulo.apispring;
+
+public class ObjetoErro {
+
+	private String Error;
+	private String code;
+
+	public String getError() {
+		return Error;
+	}
+
+	public void setError(String error) {
+		Error = error;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
